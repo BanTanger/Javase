a8ea6e55b0f4126d39f774d542588fc426f4d583
Cheney Mose <1290288968@qq.com>
2022-03-20 16:25:35 +0800

    Test 22.3.20

--- MySQL/regexp.sql
+++ MySQL/regexp.sql
@@ -42,3 +42,33 @@ WHERE prod_name REGEXP '^[0-9\\.]' # 使用定位符
 SELECT prod_name
 FROM products
 WHERE prod_name REGEXP '[0-9\\.]' # 不使用定位符
+
+SELECT CONCAT(vend_name,'(',vend_country,')')
+AS '名字(位置)'
+FROM vendors
+ORDER BY vend_name
+
+SELECT CONCAT(RTRIM(vend_name),'(',vend_country,')')
+FROM vendors
+ORDER BY vend_name
+
+SELECT NOW()
+
+SELECT vend_name,UPPER(vend_name)
+FROM vendors
+ORDER BY vend_name
+
+SELECT cust_name,cust_contact
+FROM customers
+WHERE cust_contact = 'Y.Lie'
+
+SELECT order_date
+FROM orders
+
+SELECT cust_id,order_num
+FROM orders
+WHERE DATE(order_date)='2005-09-01'
+
+SELECT cust_id,order_num
+FROM orders
+WHERE YEAR(order_date) = 2005 AND MONTH(order_date) = 9;
